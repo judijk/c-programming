8381e7e5a79b90fdd9afd3bcbf6ce7d08d6de813
judijk <37831310+judijk@users.noreply.github.com>
2018-03-27 15:51:10 +0530

    Create positive.c

---
+++ beginner level/positive.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+int k;
+printf("Enter the value of k");
+scanf("%d",&k);
+if(k>0)
+{
+printf("positive");
+}
+else
+{
+printf("negative");
+}
+return 0;
+}
